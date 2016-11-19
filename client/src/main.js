2176ff30a4ee32e8891d3bbbf291613caf14a350
Triet Phan <TrietPhan@dhcp-10-105-214-235.wireless.northwestern.private>
2016-11-18 22:23:42 -0600

    Add vue-router

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import Home from './home/Home.vue'
+
+const routes = [
+  {path: '/', component: Home}
+]
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
